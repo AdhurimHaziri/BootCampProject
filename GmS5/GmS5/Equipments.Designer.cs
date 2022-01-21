2412328ad7ce73af4e27527141139240d78f2666
AdhurimHaziri <95883390+AdhurimHaziri@users.noreply.github.com>
2022-01-22 00:31:55 +0100

    Add files via upload

---
+++ GmS5/GmS5/Equipments.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace GmS5
+{
+    partial class Equipments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Equipments));
+            this.i = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEquipName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.txtMusclesUsed = new System.Windows.Forms.TextBox();
+            this.dateTimePickerDeliveryDate = new System.Windows.Forms.DateTimePicker();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.RadioButton();
+            this.btnReset = new System.Windows.Forms.RadioButton();
+            this.ViewEquipments = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // i
+            // 
+            this.i.BackColor = System.Drawing.Color.Transparent;
+            this.i.Image = ((System.Drawing.Image)(resources.GetObject("i.Image")));
+            this.i.Location = new System.Drawing.Point(300, 43);
+            this.i.Name = "i";
+            this.i.Size = new System.Drawing.Size(164, 144);
+            this.i.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(125, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Equipment Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(136, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 16);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Muscles Used";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(139, 448);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 16);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Delivery Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(175, 494);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 16);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Cost";
+            // 
+            // txtEquipName
+            // 
+            this.txtEquipName.Location = new System.Drawing.Point(257, 190);
+            this.txtEquipName.Name = "txtEquipName";
+            this.txtEquipName.Size = new System.Drawing.Size(229, 20);
+            this.txtEquipName.TabIndex = 33;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(257, 239);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(293, 138);
+            this.txtDescription.TabIndex = 34;
+            this.txtDescription.Text = "";
+            // 
+            // txtMusclesUsed
+            // 
+            this.txtMusclesUsed.Location = new System.Drawing.Point(257, 399);
+            this.txtMusclesUsed.Name = "txtMusclesUsed";
+            this.txtMusclesUsed.Size = new System.Drawing.Size(229, 20);
+            this.txtMusclesUsed.TabIndex = 35;
+            // 
+            // dateTimePickerDeliveryDate
+            // 
+            this.dateTimePickerDeliveryDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDeliveryDate.Location = new System.Drawing.Point(257, 444);
+            this.dateTimePickerDeliveryDate.Name = "dateTimePickerDeliveryDate";
+            this.dateTimePickerDeliveryDate.Size = new System.Drawing.Size(229, 20);
+            this.dateTimePickerDeliveryDate.TabIndex = 36;
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(257, 487);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(229, 20);
+            this.txtCost.TabIndex = 37;
+            // 
+            // btnSave
+            // 
+            this.btnSave.AutoSize = true;
+            this.btnSave.Location = new System.Drawing.Point(257, 534);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(50, 17);
+            this.btnSave.TabIndex = 38;
+            this.btnSave.TabStop = true;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.CheckedChanged += new System.EventHandler(this.btnSave_CheckedChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.AutoSize = true;
+            this.btnReset.Location = new System.Drawing.Point(380, 534);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(53, 17);
+            this.btnReset.TabIndex = 39;
+            this.btnReset.TabStop = true;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.CheckedChanged += new System.EventHandler(this.btnReset_CheckedChanged);
+            // 
+            // ViewEquipments
+            // 
+            this.ViewEquipments.AutoSize = true;
+            this.ViewEquipments.Location = new System.Drawing.Point(488, 534);
+            this.ViewEquipments.Name = "ViewEquipments";
+            this.ViewEquipments.Size = new System.Drawing.Size(106, 17);
+            this.ViewEquipments.TabIndex = 40;
+            this.ViewEquipments.TabStop = true;
+            this.ViewEquipments.Text = "View Equipments";
+            this.ViewEquipments.UseVisualStyleBackColor = true;
+            this.ViewEquipments.CheckedChanged += new System.EventHandler(this.ViewEquipments_CheckedChanged);
+            // 
+            // Equipments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(768, 603);
+            this.Controls.Add(this.ViewEquipments);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.dateTimePickerDeliveryDate);
+            this.Controls.Add(this.txtMusclesUsed);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtEquipName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.i);
+            this.Name = "Equipments";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Equipments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label i;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEquipName;
+        private System.Windows.Forms.RichTextBox txtDescription;
+        private System.Windows.Forms.TextBox txtMusclesUsed;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDeliveryDate;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.RadioButton btnSave;
+        private System.Windows.Forms.RadioButton btnReset;
+        private System.Windows.Forms.RadioButton ViewEquipments;
+    }
+}
